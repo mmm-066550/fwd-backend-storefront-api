2e6cda856c704b3be4144899628101ff8dc8a384
mmm-066550 <95170213+mmm-066550@users.noreply.github.com>
2022-05-13 14:00:46 +0200

    ...

--- src/routes/api/order-products.routes.ts
+++ src/routes/api/order-products.routes.ts
@@ -1,22 +1,23 @@
 import { Router } from 'express'
 import opController from '../../controllers/orders-products.controller'
+import routeAuthGuard from '../../middlewares/authRoute'
 
 // Create Orders Products Router Instance
 const route = Router()
 
 // Create A New Order Products Router [POST:'api/order-products']
-route.post('/', opController.createNewOrderProduct)
+route.post('/', routeAuthGuard, opController.createNewOrderProduct)
 
 // Get All Orders Router [GET:'api/order-products/:id/products']
-route.get('/:id/products', opController.getAllOrderProducts)
+route.get('/:id/products', routeAuthGuard, opController.getAllOrderProducts)
 
 // Get Product Of An Order By Order ID Router [GET:'api/order-products/:id/products/:product_id']
-route.get('/:id/products/:product_id', opController.getOrderProductByID)
+route.get('/:id/products/:product_id', routeAuthGuard, opController.getOrderProductByID)
 
 // Update Product Of An Order Router [PATCH:'api/order-products/:id/products/:product_id']
-route.patch('/:id/products/:product_id', opController.updateOrderProduct)
+route.patch('/:id/products/:product_id', routeAuthGuard, opController.updateOrderProduct)
 
 // Delete Product Of An Order Router [DELETE:'api/order-products/:id/products/:product_id']
-route.delete('/:id/products/:product_id', opController.deleteOrderProduct)
+route.delete('/:id/products/:product_id', routeAuthGuard, opController.deleteOrderProduct)
 
 export default route
--- src/routes/api/orders.routes.ts
+++ src/routes/api/orders.routes.ts
@@ -1,25 +1,26 @@
 import { Router } from 'express'
 import ordersController from '../../controllers/orders.controller'
+import routeAuthGuard from '../../middlewares/authRoute'
 
 // Create Orders Router Instance
 const route = Router()
 
 // Create A New Order Router [POST:'api/orders']
-route.post('/', ordersController.createNewOrder)
+route.post('/', routeAuthGuard, ordersController.createNewOrder)
 
 // Get All Orders Router [GET:'api/orders']
-route.get('/', ordersController.getAllOrders)
+route.get('/', routeAuthGuard, ordersController.getAllOrders)
 
 // Get Order Of An User By User ID Router [GET:'api/orders/user/:user_id']
-route.get('/user/:user_id', ordersController.getAllOrdersOfUser)
+route.get('/user/:user_id', routeAuthGuard, ordersController.getAllOrdersOfUser)
 
 // Get An Order By ID Router [GET:'api/orders/:order_id']
-route.get('/:order_id', ordersController.getOrderByID)
+route.get('/:order_id', routeAuthGuard, ordersController.getOrderByID)
 
 // Update Order Router [PATCH:'api/orders/:order_id']
-route.patch('/:order_id', ordersController.updateOrder)
+route.patch('/:order_id', routeAuthGuard, ordersController.updateOrder)
 
 // Delete Order Router [DELETE:'api/orders/:order_id']
-route.delete('/:order_id', ordersController.deleteOrder)
+route.delete('/:order_id', routeAuthGuard, ordersController.deleteOrder)
 
 export default route
