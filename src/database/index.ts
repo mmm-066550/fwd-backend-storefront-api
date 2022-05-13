b5fbcbad8fa4ec9030f0388b6605cf0626178be9
mmm-066550 <95170213+mmm-066550@users.noreply.github.com>
2022-05-14 01:53:50 +0200

    Update index.ts

--- src/database/index.ts
+++ src/database/index.ts
@@ -5,13 +5,26 @@ import { Pool } from 'pg'
 dotenv.config({ path: '.env' })
 
 // Destructuring Postgress DB Credentials From Node Environment Variables
-const { POSTGRESS_DB_HOST, POSTGRESS_DB, POSTGRESS_DB_PORT, POSTGRESS_DB_USER, POSTGRESS_DB_PASS } =
-  process.env
+const {
+  POSTGRESS_DB_HOST,
+  POSTGRESS_DB,
+  POSTGRESS_DB_TEST,
+  POSTGRESS_DB_PORT,
+  POSTGRESS_DB_USER,
+  POSTGRESS_DB_PASS
+} = process.env
+
+let DB
+if (process.env.NODE_EVN === 'dev') {
+  DB = POSTGRESS_DB_TEST
+} else {
+  DB = POSTGRESS_DB
+}
 
 // Create Postgres DB Pool Instance
 const pool = new Pool({
   host: POSTGRESS_DB_HOST,
-  database: POSTGRESS_DB,
+  database: DB,
   user: POSTGRESS_DB_USER,
   password: POSTGRESS_DB_PASS,
   port: POSTGRESS_DB_PORT as unknown as number
