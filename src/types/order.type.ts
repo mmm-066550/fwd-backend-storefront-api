e123de3310716ed8964ab71c2480e840ee3c2425
mmm-066550 <95170213+mmm-066550@users.noreply.github.com>
2022-05-14 01:54:16 +0200

    Update order.type.ts

--- src/types/order.type.ts
+++ src/types/order.type.ts
@@ -3,7 +3,7 @@ import order_product from './order-products.type'
 type order = {
   id?: number | string
   status: string
-  user_id: number
+  user_id: number | string
   ordered_at?: Date
   products?: order_product[]
 }
