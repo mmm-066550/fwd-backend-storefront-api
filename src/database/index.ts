a37c3b10419ca10ba24052d297c1d12945af8d5d
mmm-066550 <95170213+mmm-066550@users.noreply.github.com>
2022-05-14 02:09:43 +0200

    Update index.ts

--- src/database/index.ts
+++ src/database/index.ts
@@ -15,7 +15,7 @@ const {
 } = process.env
 
 let DB
-if (process.env.NODE_EVN === 'dev') {
+if (process.env.NODE_EVN === 'dev' || process.env.NODE_EVN === 'development') {
   DB = POSTGRESS_DB_TEST
 } else {
   DB = POSTGRESS_DB
